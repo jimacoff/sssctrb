d2360bc80637a14397de9d26d2ced6e7b51f4bb8
Anand Srinivasan <asriniva@ubuntu.(none)>
2014-07-26 13:08:48 +0530

    Misc Changes

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -16,9 +16,17 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
       sign_in("user", confirmed_user)
       flash[:message] = "Your account was already Activated."
     else
-      self.resource = resource_class.confirm_by_token(confirmation_token)
-      sign_in("user", resource)
-      flash[:message] = "Your account has been Activated."
+      devise_confirmation_token = Devise.token_generator.digest(self, :confirmation_token, confirmation_token)
+      user_to_be_confirmed =
+          resource_class.find_by_confirmation_token(devise_confirmation_token)
+      if(user_to_be_confirmed)
+        self.resource = resource_class.confirm_by_token(confirmation_token)
+        sign_in("user", resource)
+        flash[:message] = "Your account has been Activated."
+      else
+        flash[:message] = "This is an In-valid Activation Token. Please check the token and try again."
+      end
+
     end
 
     redirect_to "/"
